641265a459c7e226a0add33f750d77aa897c463e
Erick Bailey <baileytorch@hotmail.com>
2026-09-11 18:29:57 -0300

    Aplicando migraciones

--- drf/settings.py
+++ drf/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rse-91g&c45)54g+0-q3^_$9@#^dg-g30@*fyur_xo&wk7cg!m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'drf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- incidencias/models.py
+++ incidencias/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Pais(models.Model):
+    nombre = models.CharField(max_length=60,null=False)
+    nacionalidad = models.CharField(max_length=20,null=False)
+    iso_2 = models.CharField(max_length=2,null=False)
+    iso_3 = models.CharField(max_length=3,null=False)
+    habilitado = models.BooleanField(default=True)
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100,null=True)
+    pseudonimo = models.CharField(max_length=50,null=True)
+    nacionalidad = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    fecha_nacimiento = models.DateField(null=True)
+    fecha_defuncion = models.DateField(null=True)
+    habilitado = models.BooleanField(default=True)
